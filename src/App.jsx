b37a3923536d62bd32fb449c19b9b86754ff8515
Minhaz Chowdhury <chowdhuryminhaz021@gmail.com>
2023-11-04 23:14:53 +0600

    RETRIVED

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Clock from './components/Clock'
+import ClockList from './components/clocklist'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const nums=[1,2,3,4]
 
   return (
     <>
       <div>
-        <Clock/>
+        <ClockList quantities={nums}/>
       </div>
     </>
   )
