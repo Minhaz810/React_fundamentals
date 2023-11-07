d188593d9df7d0641140ebf92391df2d3a473a29
Minhaz Chowdhury <chowdhuryminhaz021@gmail.com>
2023-11-08 01:07:41 +0600

    updated

--- src/components/HoverCounter.jsx
+++ src/components/HoverCounter.jsx
@@ -1,3 +1,39 @@
+import WithCounter from "./HOC/withCounter"
+const HoverCounter=(props)=>{
+    const {count,increamentCount}=props
+    return (
+    <div>
+        <h1 onMouseOver={increamentCount}>
+            Hovered {count} times
+        </h1>
+    </div>
+    )
+}
+export default WithCounter(HoverCounter)
+
+
+/* 
+ধরা যাক client আমার কাছে কিছুদিন পর আরেকটা compnent চাইলো যে একটা inputfield দিতে হবে,যেটার মধ্যে কয়বার Type 
+করা হয়েছে সেটার count রাখতে হবে। সেই ক্ষেত্রে আমরা চাইলেই পারি নতুন component বানাতে,কিন্তু বারবার এরকম করতে থাকলে
+আমাদের project আর scalable থাকবে না।
+
+একটা probable solution হচ্ছে আমরা Lifting State Up method অনুযায়ী একটা parent এই state গুলাকে দিতে পারি,
+কিন্তু সেই ক্ষেত্রে আমাদের দুইটা state maintain করতে হবে,একটা হচ্ছে ClickCount এবং আরেকটা হচ্ছে HoverCount.
+এবং আমাদের method ও দুইটা লাগবে,এরপর আমরা সেগুলোকে props আকারে পাঠাতে পারবো আমাদের component এ।
+
+কিন্তু প্রবলেম টা হচ্ছে,এক্ষেত্রে আমাদের ClickCounter ও HoverCounter দুইটাই আছে সরাসরি APP এর child হিসেবে,কিন্তু যদি এমন হতো,
+
+hoverCounter টা আরো দুইটা component এর মধ্যে nested,সেই ক্ষেত্রে props যদি আমরা App থেকে HoverCounter এ পাঠাতে চেতাম তাহলে আরো
+দুইটা Component এর ভেতর দিয়ে props গুলোকে Passed হতে হতো।
+
+এখানেই চলে আসে Higher Order Component
+
+React এর Definition অনুযায়ী Higher-Order Component হচ্ছে একটা function যেটা একটা component
+কে parameter হিসেবে নিবে,এবং return করে দিবে একটা নতুন component কে।
+
+const NewComponent=HigherOrderComponent(OriginalComponent)
+
+
 import React from "react";
 
 class HoverCounter extends React.Component{
@@ -22,28 +58,5 @@ class HoverCounter extends React.Component{
     }
 }
 export default HoverCounter;
-
-
-/* 
-ধরা যাক client আমার কাছে কিছুদিন পর আরেকটা compnent চাইলো যে একটা inputfield দিতে হবে,যেটার মধ্যে কয়বার Type 
-করা হয়েছে সেটার count রাখতে হবে। সেই ক্ষেত্রে আমরা চাইলেই পারি নতুন component বানাতে,কিন্তু বারবার এরকম করতে থাকলে
-আমাদের project আর scalable থাকবে না।
-
-একটা probable solution হচ্ছে আমরা Lifting State Up method অনুযায়ী একটা parent এই state গুলাকে দিতে পারি,
-কিন্তু সেই ক্ষেত্রে আমাদের দুইটা state maintain করতে হবে,একটা হচ্ছে ClickCount এবং আরেকটা হচ্ছে HoverCount.
-এবং আমাদের method ও দুইটা লাগবে,এরপর আমরা সেগুলোকে props আকারে পাঠাতে পারবো আমাদের component এ।
-
-কিন্তু প্রবলেম টা হচ্ছে,এক্ষেত্রে আমাদের ClickCounter ও HoverCounter দুইটাই আছে সরাসরি APP এর child হিসেবে,কিন্তু যদি এমন হতো,
-
-hoverCounter টা আরো দুইটা component এর মধ্যে nested,সেই ক্ষেত্রে props যদি আমরা App থেকে HoverCounter এ পাঠাতে চেতাম তাহলে আরো
-দুইটা Component এর ভেতর দিয়ে props গুলোকে Passed হতে হতো।
-
-এখানেই চলে আসে Higher Order Component
-
-React এর Definition অনুযায়ী Higher-Order Component হচ্ছে একটা function যেটা একটা component
-কে parameter হিসেবে নিবে,এবং return করে দিবে একটা নতুন component কে।
-
-const NewComponent=HigherOrderComponent(OriginalComponent)
-
 
 */
--- src/components/clickCounter.jsx
+++ src/components/clickCounter.jsx
@@ -1,3 +1,32 @@
+import WithCounter from "./HOC/withCounter"
+const ClickCounter=(props)=>{
+    const {count,increamentCount}=props
+    return (
+    <div>
+        <button type="button" onClick={increamentCount}>
+            Clicked {count} times
+        </button>
+    </div>
+    )
+}
+export default WithCounter(ClickCounter)
+
+/* 
+this.setState((state)=>({
+            count:state.count+1 //এখানে state টা হচ্ছে আমাদের previous state যেখনে counter এর ভ্যালু 0 ছিলো
+        })
+        )
+        this.setState((state)=>()) এভাবে করতে হবে। (state)=>() bracket না দিলে কাজ করবে না।
+
+==>setState is called with a function that takes the previous state as its argument.
+
+==>The function returns an object with the updated state, where count is incremented by 1. {This object is wrapped in parentheses to indicate that it's a single object literal.}
+
+==>The updated state is then passed to setState for updating.
+
+With this corrected code, when you call this.incrementCount(), it should properly increment the count property in the component's state.
+
+
 import React from "react";
 
 class ClickCounter extends React.Component{
@@ -23,19 +52,5 @@ class ClickCounter extends React.Component{
 }
 export default ClickCounter;
 
-
-/* 
-this.setState((state)=>({
-            count:state.count+1 //এখানে state টা হচ্ছে আমাদের previous state যেখনে counter এর ভ্যালু 0 ছিলো
-        })
-        )
-        this.setState((state)=>()) এভাবে করতে হবে। (state)=>() bracket না দিলে কাজ করবে না।
-
-==>setState is called with a function that takes the previous state as its argument.
-
-==>The function returns an object with the updated state, where count is incremented by 1. {This object is wrapped in parentheses to indicate that it's a single object literal.}
-
-==>The updated state is then passed to setState for updating.
-
-With this corrected code, when you call this.incrementCount(), it should properly increment the count property in the component's state.
+আমরা Clickcounter থেকে state গুলো যেহেতু HOC তে নিয়ে গিয়েছি,তাই আমরা Click কে একটা functional component বানিয়ে ফেলি।
 */ 
