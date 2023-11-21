6d7725ab311bdecbddb6091331e91a947349b8d2
Minhaz Chowdhury <chowdhuryminhaz021@gmail.com>
2023-11-22 00:19:24 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
-
+import Logo from './components/logo'
+import './assets/css/global.css'
 function App() {
   
 
   return (
     <>
-    
+      <div className='App'>
+        <h1>Learn With Sumit</h1>
+        <Logo/>
+      </div>
     </>
   )
 }
