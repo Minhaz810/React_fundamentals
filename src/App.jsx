cddc2a22a76eb91ff016f30d16a48bd12ae34b48
Minhaz Chowdhury <chowdhuryminhaz021@gmail.com>
2023-11-21 22:16:27 +0600

    main changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Layout2 from './components/Layout2'
+
 
 function App() {
   
 
   return (
     <>
-     <Layout2/>
+    
     </>
   )
 }
